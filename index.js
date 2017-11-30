484539e687fa4ca00cc64c9779be0eae5bab3e0e
Lily2point0 <lily.madar@ft.com>
2017-11-30 13:43:58 +0000

    check environment for env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -5,12 +6,13 @@ const homepagecontent = require('./bin/lib/homepage');
 const Utils  = require('./bin/lib/utils');
 
 const pollInterval = Utils.minutesToMs(process.env.POLLING_INTERVAL_MINUTES);
+console.log(pollInterval);
 
 async function getContent() {
 	const imageData =  await homepagecontent.frontPage();
 	console.log('UK HOMEPAGE', imageData);
 }
 
-getContent();
+// getContent();
 
 setTimeout(getContent, pollInterval);
