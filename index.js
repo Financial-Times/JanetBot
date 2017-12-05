7b3440c03dea457f41ab39056fbda2ac6307ea7b
Lily2point0 <lily.madar@ft.com>
2017-12-05 16:24:48 +0000

    add slackbot integration

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const homepagecontent = require('./bin/lib/homepage');
 const Utils  = require('./bin/lib/utils');
+const janetBot = require('./bin/lib/bot');
 
 const pollInterval = Utils.minutesToMs(process.env.POLLING_INTERVAL_MINUTES);
 
@@ -13,6 +14,8 @@ async function getContent() {
 
 	const internationalImageData =  await homepagecontent.frontPage('international');
 	console.log('INT HOMEPAGE', internationalImageData.length, internationalImageData);
+
+	janetBot.warn(`There are ${imageData.length} images on the UK Homepage & ${internationalImageData.length} on the International homepage, including local variations.`);
 }
 
 getContent();
