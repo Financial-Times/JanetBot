660d8c6a0665c447aa9ad80f5fa0b26b8fb4f504
Lily2point0 <lily.madar@ft.com>
2018-01-09 11:05:00 +0000

    add mock results

--- index.js
+++ index.js
@@ -10,12 +10,22 @@ const pollInterval = Utils.minutesToMs(process.env.POLLING_INTERVAL_MINUTES);
 
 async function getContent() {
 	const imageData =  await homepagecontent.frontPage();
-	console.log('UK HOMEPAGE', imageData.length, imageData);
+	// console.log('UK HOMEPAGE', imageData.length, imageData);
+	console.log(await analyseContent(imageData));
 
 	// const internationalImageData =  await homepagecontent.frontPage('international');
 	// console.log('INT HOMEPAGE', internationalImageData.length, internationalImageData);
 
 	// janetBot.warn(`There are ${imageData.length} images on the UK Homepage & ${internationalImageData.length} on the International homepage, including local variations.`);
+}
+
+async function analyseContent(content) {
+	for(let i = 0; i < content.length; ++i) {
+		//Add mock result until API ready
+		content[i].isWoman = (Math.floor(Math.random()*1000)%5 === 0);
+	}
+
+	return content;
 }
 
 getContent();
